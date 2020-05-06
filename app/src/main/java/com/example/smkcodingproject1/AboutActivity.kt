988aab65670c7558b7941183534a19c624f4c664
frostbite202 <ardiansyahrafi4@gmail.com>
2020-05-06 16:32:01 +0700

    Penambahan dial di about dan front end, serta remake karna ada error

--- app/src/main/java/com/example/smkcodingproject1/AboutActivity.kt
+++ app/src/main/java/com/example/smkcodingproject1/AboutActivity.kt
@@ -1,12 +1,26 @@
 package com.example.smkcodingproject1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        tv_abouttelp.setOnClickListener{ dialPhone(tv_abouttelp.text.toString())}
+    }
+
+    private fun dialPhone(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
