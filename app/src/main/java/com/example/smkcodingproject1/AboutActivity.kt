3a2b76267eb970b94b7e73fdcea704e9c8dfc921
frostbite202 <ardiansyahrafi4@gmail.com>
2020-05-06 21:53:58 +0700

    Menambahkan Maps location pada halaman About

--- app/src/main/java/com/example/smkcodingproject1/AboutActivity.kt
+++ app/src/main/java/com/example/smkcodingproject1/AboutActivity.kt
@@ -4,15 +4,34 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
+
+    //maps
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap : GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
         tv_abouttelp.setOnClickListener{ dialPhone(tv_abouttelp.text.toString())}
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback{
+            googleMap = it
+
+            val location1 = LatLng(-7.972048 , 112.627545)
+            googleMap.addMarker(MarkerOptions().position(location1).title("My Location"))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1, 10f))
+        })
     }
 
     private fun dialPhone(phoneNumber: String) {
@@ -23,4 +42,7 @@ class AboutActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+    
+
+
 }
